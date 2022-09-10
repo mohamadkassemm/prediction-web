e562fea75a3d60e34a51f35026377baa63aeb12c
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-10 09:41:39 +0300

    addition of fetch functions to API's

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,34 @@
 window.onload=() => {
-   console.log("hi")
+   const image =document.getElementById("image-placement")
+   const predictButton =document.getElementById("predict-btn")
+   const Nationality =document.getElementById("nationality")
+   const Age =document.getElementById("age")
+   const Gender =document.getElementById("gender")
+
+   fetch("https://dog.ceo/api/breeds/image/random")
+   .then(res => res.json())
+   .then(data => {
+       image.innerHTML = `<img class="image-result" src="${data.message}"/> `
+   })
+
+    predictButton.addEventListener("click",prediction)
+
+    function prediction(){
+        fetch("https://api.nationalize.io/?name=mohamad")
+            .then(res => res.json())
+            .then(data => {
+                Nationality.innerHTML = `<p> Nationality:${data.country_id}</p>`
+            })
+        fetch("https://api.agify.io/?name=nour")
+            .then(res => res.json())
+            .then(data => {
+                Age.innerHTML = `<p> Age:${data.age}</p>`
+            }) 
+        fetch("https://api.genderize.io/?name=rio")
+            .then(res => res.json())
+            .then(data => {
+                Gender.innerHTML = `<p> Gender:${data.gender}</p>`
+            })
+    }
+
 }
