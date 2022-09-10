45474dabd1dbd10dcd5b52d9b374046788cb4251
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-10 09:00:02 +0300

    addition of some frontend designs

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,3 @@
 window.onload=() => {
-    const body=document.getElementById("body")
-
-    body.style.backgroundImage= URL("https://images.dog.ceo//breeds//setter-irish//duke2.jpg");
+   console.log("hi")
 }
